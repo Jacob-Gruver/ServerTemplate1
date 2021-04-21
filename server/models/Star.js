cb437397212cecaad4d55d0cb738456cf1c2a123
Jacob <gruvinbro@gmail.com>
2021-04-21 10:11:02 -0600

    init

--- server/models/Star.js
+++ server/models/Star.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const Star = new Schema (
+const Star = new Schema(
     {
-        name: { type: String, required: true},
-        mass: { type: Number, required: true},
-        planetpop: { type: Number, required: true},
+        name: { type: String, required: true },
+        mass: { type: Number, required: true },
+        planetpop: { type: Number, required: true },
+        galaxy: { type: ObjectId, ref: 'Galaxy', required: true },
     }
 );
+
+
 
 export default Star;
