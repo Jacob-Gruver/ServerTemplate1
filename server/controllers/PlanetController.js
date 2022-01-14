1ff3294bccb0f47d523928394e121a2829f32fd4
Jacob-Gruver <gruvinbro@gmail.com>
2022-01-14 16:00:32 -0700

    init

--- server/controllers/PlanetController.js
+++ server/controllers/PlanetController.js
@@ -1,3 +1,19 @@
 import express, { query } from 'express'
 import BaseController from '../utils/BaseController'
 import { planetService } from '../services/PlanetService'
+
+export class PlanetController extends BaseController {
+    constructor() {
+        super("api/planets");
+        this.router
+            .get("", this.getAll)
+    }
+
+    async getAll(req, res, next) {
+        try {
+            return res.send(await planetService.find(req.query))
+        } catch (error) {
+            next(error)
+        }
+    }
+}
