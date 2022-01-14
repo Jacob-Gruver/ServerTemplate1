4b20f939378abec17eb4f71d1a424532bac5579a
Jacob-Gruver <gruvinbro@gmail.com>
2022-01-14 15:39:30 -0700

    init

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,15 @@
 import ValueSchema from "../models/Value";
+import UniverseSchema from '../models/Universe.js'
 import GalaxySchema from "../models/Galaxy.js";
 import StarSchema from "../models/Star.js";
+import PlanetSchema from '../models/'
 
 import mongoose from "mongoose";
 
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
+
+  Universe = mongoose.model("Universe", UniverseSchema)
 
   Galaxies = mongoose.model("Galaxy", GalaxySchema);
 
