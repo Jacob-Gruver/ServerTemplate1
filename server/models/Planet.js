3e0a00b3db053c682c8eda6545eb54e0e39ab978
Jacob-Gruver <gruvinbro@gmail.com>
2022-01-20 14:30:10 -0700

    init

--- server/models/Planet.js
+++ server/models/Planet.js
@@ -6,7 +6,7 @@ const Planet = new Schema(
         name: { type: String, required: true, default: 'unknown' },
         mass: { type: Number, required: true },
         moons: { type: Number, required: true, default: 0 },
-        government: { type: String, enum: ['dictatorship', 'republic', 'empirical', 'mixed', 'primitive', 'n/a'], default: 'n/a' },
+        government: { type: String, required: true, enum: ['dictatorship', 'republic', 'empirical', 'mixed', 'primitive', 'n/a'], default: 'n/a' },
         species: { type: String, required: true, default: 'n/a' }
     }
 )
