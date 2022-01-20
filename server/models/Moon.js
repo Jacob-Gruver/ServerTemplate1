5434e51d9f6df4cde3379729818a743bd36beb06
Jacob-Gruver <gruvinbro@gmail.com>
2022-01-20 14:51:16 -0700

    init

--- server/models/Moon.js
+++ server/models/Moon.js
@@ -6,11 +6,12 @@ const Moon = new Schema(
     {
         name: { type: String, required: true, default: 'unknown' },
         mass: { type: Number, required: true },
-
-
+        craters: { type: Number, required: true, default: 0 }
     }
 
 )
 // define constant object with a new instance of const Schema
-// 
+
+
+export default Moon
 
