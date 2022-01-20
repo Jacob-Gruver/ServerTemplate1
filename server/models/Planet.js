58f678f7eddd904e8c125a939f49ce191b397b20
Jacob-Gruver <gruvinbro@gmail.com>
2022-01-20 14:19:41 -0700

    init

--- server/models/Planet.js
+++ server/models/Planet.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema
 
 const Planet = new Schema(
     {
-        name: { type: String, required: true },
+        name: { type: String, required: true, default: 'unknown' },
         mass: { type: Number, required: true },
-        moons: { type: Number, required: true, default: 'n/a' },
+        moons: { type: Number, required: true, default: 0 },
         government: { type: String, enum: ['dictatorship', 'republic', 'empirical', 'mixed', 'primitive', 'n/a'], default: 'n/a' },
         species: { type: String, required: true, default: 'n/a' }
     }
