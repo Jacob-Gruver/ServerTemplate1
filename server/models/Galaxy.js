739d73b8faacb0c37e7eec8b8ddfa1ac6a609ee9
Jacob-Gruver <gruvinbro@gmail.com>
2022-02-03 14:35:37 -0700

    init

--- server/models/Galaxy.js
+++ server/models/Galaxy.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import { generateCodeName } from "../utils/GenerateCodeName";
+// import { generateCodeName } from "../utils/GenerateCodeName";
 const Schema = mongoose.Schema;
 const objectId = mongoose.Schema.Types.ObjectId
 
 const Galaxy = new Schema(
     {
-        name: { type: String, required: true, default: generateCodeName('g') },
+        name: { type: String, required: true, },
         type: { type: String, required: true, enum: ['spiral', 'barred spiral', 'elliptical', 'peculiar', 'irregular', 'lenticular'] },
         starpop: { type: Number, required: true },
         universeId: { type: objectId, ref: 'Universe', required: true }
--- server/models/Universe.js
+++ server/models/Universe.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import { generateCodeName } from "../utils/GenerateCodeName"
+// import { generateCodeName } from "../utils/GenerateCodeName"
 const Schema = mongoose.Schema;
 
 const Universe = new Schema(
     {
-        name: { type: String, required: true, default: generateCodeName('u') },
+        name: { type: String, required: true },
         galacticWeb: { type: String, required: true, enum: ['spiral', 'tree', 'web'], default: 'web' },
         galaxypop: { type: Number, required: true }
     }
