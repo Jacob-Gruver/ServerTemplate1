b46bd9b552784955862444e9f72e7c954f4642b2
Jacob-Gruver <gruvinbro@gmail.com>
2022-02-04 09:44:52 -0700

    init

--- server/controllers/GalaxyController.js
+++ server/controllers/GalaxyController.js
@@ -1,4 +1,3 @@
-import express, { query } from "express";
 import BaseController from "../utils/BaseController";
 import { galaxyService } from "../services/GalaxyService";
 // import { starService } from "../services/StarService"
