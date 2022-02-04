cc02d8ac30f2d123ddd63aa06f259e45d9706ea0
Jacob-Gruver <gruvinbro@gmail.com>
2022-02-04 15:36:14 -0700

    init

--- server/controllers/UniverseController.js
+++ server/controllers/UniverseController.js
@@ -1,4 +1,3 @@
-import express, { query } from "express"
 import BaseController from "../utils/BaseController"
 import { universeService } from '../services/UniverseService'
 import { galaxyService } from "../services/GalaxyService"
