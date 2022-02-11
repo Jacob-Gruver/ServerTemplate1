6c980f5077d3d07c7d92793583379e672ba68397
Jacob-Gruver <gruvinbro@gmail.com>
2022-02-11 16:02:32 -0700

    init

--- server/models/Star.js
+++ server/models/Star.js
@@ -6,8 +6,8 @@ const Star = new Schema(
     {
         name: { type: String, required: true },
         mass: { type: Number, required: true },
-        color: { type: String, required: true, enum: ['blue', 'white', 'yellow', 'Orange', 'Red'] },
-        type: { type: String, required: true, enum: ['main sequence', 'dwarf', 'giant', 'supergiant'], default: 'main sequence' },
+        color: { type: String, required: true, enum: ['blue', 'white', 'yellow', 'orange', 'red'] },
+        type: { type: String, required: true, enum: ['main sequence', 'dwarf', 'giant', 'supergiant', 'binary'], default: 'main sequence' },
         planetpop: { type: Number, required: true },
         galaxyId: { type: ObjectId, ref: 'Galaxy', required: true }
     }
