1300154047bc1549904e2b47c597df35e40b2c8a
Jacob-Gruver <gruvinbro@gmail.com>
2022-02-11 17:28:53 -0700

    init

--- server/controllers/PlanetController.js
+++ server/controllers/PlanetController.js
@@ -1,35 +1,35 @@
-// import BaseController from '../utils/BaseController'
-// import { planetService } from '../services/PlanetService'
+import BaseController from '../utils/BaseController'
+import { planetService } from '../services/PlanetService'
 
-// export class PlanetController extends BaseController {
-//     constructor() {
-//         super("api/planets");
-//         this.router
-//             .get("", this.getAll)
-//             .post("", this.create)
-//     }
+export class PlanetController extends BaseController {
+    constructor() {
+        super("api/planets");
+        this.router
+            .get("", this.getAll)
+            .post("", this.create)
+    }
 
-//     async getAll(req, res, next) {
-//         try {
-//             return res.send(await planetService.find(req.query))
-//         } catch (error) {
-//             next(error)
-//         }
-//     }
+    async getAll(req, res, next) {
+        try {
+            return res.send(await planetService.find(req.query))
+        } catch (error) {
+            next(error)
+        }
+    }
 
-//     async get(req, res, next) {
-//         try {
-//             return res.send(await planetService.findOne(req.params.id))
-//         } catch (error) {
-//             next(error)
-//         }
-//     }
+    async get(req, res, next) {
+        try {
+            return res.send(await planetService.findOne(req.params.id))
+        } catch (error) {
+            next(error)
+        }
+    }
 
-//     async create(req, res, next) {
-//         try {
-//             return res.send(await planetService.create(req.body))
-//         } catch (error) {
-//             next(error)
-//         }
-//     }
-// }
+    async create(req, res, next) {
+        try {
+            return res.send(await planetService.create(req.body))
+        } catch (error) {
+            next(error)
+        }
+    }
+}
