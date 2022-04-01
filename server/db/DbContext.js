62cc6d120b02f739fe6cb7d88f6ea3ee2d4de616
Jacob-Gruver <gruvinbro@gmail.com>
2022-04-01 16:40:22 -0600

    init

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,23 +1,8 @@
-import UniverseSchema from '../models/Universe.js'
-import GalaxySchema from "../models/Galaxy.js"
-import StarSchema from "../models/Star.js"
-import NebulaSchema from "../models/Nebula.js"
-import PlanetSchema from '../models/Planet.js'
 import MoonSchema from '../models/Moon.js'
 
 import mongoose from "mongoose";
 
 class DbContext {
-
-  Universes = mongoose.model("Universe", UniverseSchema)
-
-  Galaxies = mongoose.model("Galaxy", GalaxySchema)
-
-  Stars = mongoose.model("Star", StarSchema)
-
-  Nebulas = mongoose.model("Nebula", NebulaSchema)
-
-  Planets = mongoose.model("Planet", PlanetSchema)
 
   Moons = mongoose.model("Moon", MoonSchema)
 
