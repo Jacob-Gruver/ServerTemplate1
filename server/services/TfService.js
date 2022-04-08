444d0abc948c0448418a220a13bef5fed1ce90fe
Jacob-Gruver <gruvinbro@gmail.com>
2022-04-08 11:51:41 -0600

    init

---
+++ server/services/TfService.js
@@ -0,0 +1,18 @@
+import { dbContext } from '../db/DbContext'
+// import { BadRequest } from '../utils/Errors'
+
+class TfService {
+    async getRobots(query = {}) {
+        return await dbContext.Transformer.find(query)
+    }
+
+    async getOne(id) {
+        return await dbContext.Transformer.findById(id)
+    }
+
+    async create(body) {
+        return await dbContext.Transformer.create(body)
+    }
+}
+
+export const tfService = new TfService()
