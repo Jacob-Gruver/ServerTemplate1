79af968b02f6ba5dcc71e0e4f7543c4ae823d3a4
Jacob-Gruver <gruvinbro@gmail.com>
2022-04-08 11:49:09 -0600

    init

---
+++ server/controllers/TfController.js
@@ -0,0 +1,39 @@
+import { transformerService } from '../services/TfService'
+import BaseController from '../utils/BaseController'
+
+export class TransformerController extends BaseController {
+    constructor() {
+        super('api/transformers')
+        this.router
+            .get('', this.getRobots)
+            .get('/:id', this.getOne)
+            .post('', this.create)
+    }
+
+    async getRobots(req, res, next) {
+        try {
+            const roBots = await transformerService.getRobots(req.query)
+            return res.send(roBots)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getOne(req, res, next) {
+        try {
+            const roBot = await transformerService.getOne(req.params.id)
+            return res.send(roBot)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async create(req, res, next) {
+        try {
+            const newBot = await transformerService.create(req.body)
+            res.send(newBot)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
---
+++ server/models/Transformer.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const Transformer = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        form: { type: String, required: true },
+        specs: {
+            strength: { type: Number, required: true, default: 0 },
+            intelligence: { type: Number, required: true, default: 0 },
+            speed: { type: Number, required: true, default: 0 },
+            endurance: { type: Number, required: true, default: 0 },
+            rank: { type: Number, required: true, default: 0 },
+            courage: { type: Number, required: true, default: 0 },
+            firepower: { type: Number, required: true, default: 0 },
+            skill: { type: Number, required: true, default: 0 }
+        },
+        function: { type: String, required: true, default: 'N/A' },
+        affiliation: { type: Boolean, required: true, default: false },
+        // imgURL: { type: String, required: true, default: 'https://clipartart.com/images/free-clipart-black-and-white-electronic-cog.jpg' }
+    }
+)
