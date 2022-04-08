0d919888d7ec5a79fb4061824524ab9024e35d6e
Jacob-Gruver <gruvinbro@gmail.com>
2022-04-08 11:55:36 -0600

    init

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,8 @@
-import MoonSchema from '../models/Moon.js'
 import TfSchema from '../models/Transformer'
 
 import mongoose from "mongoose";
 
 class DbContext {
-
-  Moons = mongoose.model("Moon", MoonSchema)
 
   Transformer = mongoose.model("Transformer", TfSchema)
 
